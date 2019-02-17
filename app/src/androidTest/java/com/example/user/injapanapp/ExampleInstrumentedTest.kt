4f0492381ef9fc87b4cb3b70921117c3c56960e8
Pankratov Mikhail <beardykun@yahoo.com>
2019-02-17 11:03:58 +0900

    added addTask and deleteTask Instrumental Tests

--- app/src/androidTest/java/com/example/user/injapanapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/user/injapanapp/ExampleInstrumentedTest.kt
@@ -1,19 +1,28 @@
 package com.example.user.injapanapp
 
 import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.Espresso.onData
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.contrib.RecyclerViewActions
+import android.support.test.espresso.matcher.RootMatchers.isDialog
 import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import android.support.v7.widget.RecyclerView
+import com.example.user.injapanapp.database.TaskObject
+import com.example.user.injapanapp.ui.adapter.MainAdapter
 import com.example.user.injapanapp.ui.main_activity.MainActivity
-
+import org.hamcrest.CoreMatchers.*
+import org.hamcrest.Matchers.hasEntry
+import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -25,6 +34,7 @@ class ExampleInstrumentedTest {
 
     @get:Rule
     val activityRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
@@ -33,9 +43,34 @@ class ExampleInstrumentedTest {
     }
 
     @Test
-    fun addTask(){
+    @LargeTest
+    fun addTask() {
         onView(withId(R.id.mainAddTask)).perform(click())
+        onView(withId(R.id.createTaskNumberET)).perform(click()).perform(typeText("1111111"))
+        onView(withId(R.id.createTaskTypeTV)).perform(click())
+        onData(allOf(`is`(instanceOf(String::class.java)), `is`("TEST"))).perform(click())
+        onView(withId(R.id.createTaskPriceTV)).perform(click())
+        onData(allOf(`is`(instanceOf(String::class.java)), `is`("FOC"))).perform(click())
+        onView(withId(R.id.createTaskShelfET)).perform(click()).perform(typeText("1111.11"))
+            .perform(closeSoftKeyboard())
+        onView(withId(R.id.createTaskPriorityTV)).perform(click())
+        onData(allOf(`is`(instanceOf(String::class.java)), `is`("HIGH"))).perform(click())
+        onView(withId(R.id.createTaskDescriptionET)).perform(typeText("I'm very much in love with my precious Kae-hime!"))
+            .perform(closeSoftKeyboard())
+        onView(withId(R.id.createTaskBtn)).perform(click())
+    }
 
-        onView(withId(R.id.createTaskBtn)).check(matches(isDisplayed())).perform(click())
+    @Test
+    fun deleteTask() {
+        onView(withId(R.id.mainRecyclerView)).perform(
+            RecyclerViewActions.scrollToPosition<MainAdapter.MainViewHolder>(
+                7
+            )
+        ).perform(
+            RecyclerViewActions.actionOnItem<MainAdapter.MainViewHolder>(
+                hasDescendant(withText("R-1111111")), longClick()
+            )
+        )
+        onView(withText("OK")).inRoot(isDialog()).check(matches(isDisplayed())).perform(click())
     }
 }
